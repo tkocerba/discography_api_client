d84658dae8c821630baa841157f9ea75caf438cb
Kocerba Tomasz <tkocerba@pl.grupa.iti>
2017-11-01 17:02:36 +0100

    Refactoring

--- index.rb
+++ index.rb
@@ -2,30 +2,83 @@ require 'json'
 require "awesome_print"
 require "faraday"
 
-conn = Faraday.new(:url => 'https://api.discogs.com') do |faraday|
-  faraday.request  :url_encoded
-  faraday.adapter  Faraday.default_adapter
-  faraday.authorization :Discogs, "key=WOhynZfcesgDaTBDWDsn, secret=bsNSUjBpoQTnvwtEAMdCnXDSEGwrqDvk"
+
+class JSONClient
+  def initialize(root_url)
+    @root_url = root_url
+  end
+
+  def get(path, params = {})
+    response = connection.get path, params do |request|
+      request.headers['Content-Type'] = "application/json"
+    end
+
+    body = response.body
+    json = JSON.parse(body)
+    json
+  end
+
+  def authenticate(auth_type, auth_content)
+    @authentication = [auth_type, auth_content]
+  end
+
+  private
+
+  def connection
+    @connection ||= Faraday.new(:url => @root_url) do |faraday|
+      faraday.request  :url_encoded
+      faraday.adapter  Faraday.default_adapter
+      faraday.authorization @authentication.first, @authentication.last if @authentication
+    end
+  end
 end
 
-response = conn.get '/database/search', {q: 'Master of Puppets'} do |request|
-  request.headers['Content-Type'] = "application/json"
+class DiscogsClient
+  def initialize
+    @json_client = JSONClient.new('https://api.discogs.com')
+    @json_client.authenticate(:Discogs, "key=WOhynZfcesgDaTBDWDsn, secret=bsNSUjBpoQTnvwtEAMdCnXDSEGwrqDvk")
+  end
+
+  def search(phrase)
+    json = @json_client.get '/database/search', {q: phrase}
+    results = json['results']
+    release = results.find { |e| e['type'] == 'release' && e['format'].include?('CD') && e['format'].include?('Album') }
+    release
+  end
+
+  def find_album(album_url)
+    @json_client.get(album_url)
+  end
 end
 
-json = JSON.parse(response.body)
-results = json['results']
-release = results.find { |e| e['type'] == 'release' && e['format'].include?('CD') && e['format'].include?('Album') }
+class AlbumFormatter
+  def initialize(album)
+    @album = album
+  end
 
-url = release['resource_url']
-response = conn.get(url)
-album = JSON.parse(response.body)
+  def print_album
+    puts "Tytuł: #{@album['title']}"
+    puts "Artysta: #{@album['artists'].first['name']}"
+    puts "Gatunek #{@album['genres'].join(', ')}"
+    puts "Styl #{@album['styles'].join(', ')}"
 
-puts "Tytuł: #{album['title']}"
-puts "Artysta: #{album['artist']}"
-puts "Gatunek #{album['genres'].join(', ')}"
-puts "Styl #{album['styles'].join(', ')}"
+    puts "Lista utworów:"
+    @album['tracklist'].each do |track|
+      puts "#{track['position']}\t[#{track['duration']}]\t#{track['title']}"
+    end
+  end
+end
 
-puts "Lista utworów:"
-album['tracklist'].each do |track|
-  puts "#{track['position']}\t[#{track['duration']}]\t#{track['title']}"
+class DiscogsApp
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def run
+    client = DiscogsClient.new
+    release = client.search @phrase
+    url = release['resource_url']
+    album = client.find_album(url)
+    AlbumFormatter.new(album).print_album
+  end
 end
